a3e6f77a21aee7c4dcc6d20e7b8a83e82d20857a
Guillaume Pinchon <Guillaume Pinchon>
2016-11-22 13:51:12 +0100

    Removed online babylon.js source because of security issues

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 require.config({
 	paths: {
 		jquery : 'libs/jquery',
-		babylon : navigator.onLine ? 'http://babylonjs.com/babylon' : 'libs/babylon',
+		babylon : 'libs/babylon',
 		cannon : 'libs/cannon.min'
 	},
 	shim: {
